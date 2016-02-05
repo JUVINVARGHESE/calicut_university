b386f47d09a8053fcab26a80b018ad898a1453c2
JUVINVARGHESE <juvivarghese94@gmail.com>
2016-02-05 17:22:51 +0530

    first commit

---
+++ myuniversity/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+from register.views import Home
+
+urlpatterns = [
+    url(r'^$',Home.as_view(),name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+    # Examples:
+    # url(r'^$', 'myuniversity.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+
+]
